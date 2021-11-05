b53187c82626a8d65293a9e607dd60236f08c435
Khrystyna <khrystynavysotska@gmail.com>
2021-11-05 23:28:45 +0200

    GCP configs file

---
+++ configs/gcp_configs.py
@@ -0,0 +1,14 @@
+import os
+
+DEVICE_ID = os.environ.get('DEVICE_ID')
+REGION = os.environ.get('REGION')
+PROJECT_ID = os.environ.get('PROJECT_ID')
+REGISTRY_ID = os.environ.get('REGISTRY_ID')
+PRIVATE_KEY_FILE_PATH = "./rsa_private.pem"
+CERTIFICATES_FILE_PATH = "./roots.pem"
+
+STATE_TOPIC = "state"
+DEFAULT_TOPIC = "events"
+LOGS_TOPIC = "events/logs"
+TELEMETRY_TOPIC = "events/telemetry"
+
