086c523dd3bc2be77ae3a2f7fed96ce2190cccf6
Khrystyna <khrystynavysotska@gmail.com>
2021-11-06 03:44:45 +0200

    Fixed file path

--- configs/gcp_configs.py
+++ configs/gcp_configs.py
@@ -5,7 +5,7 @@ REGION = os.environ.get('REGION')
 PROJECT_ID = os.environ.get('PROJECT_ID')
 REGISTRY_ID = os.environ.get('REGISTRY_ID')
 PRIVATE_KEY_FILE_PATH = os.environ.get('PRIVATE_KEY_FILE_PATH')
-CERTIFICATES_FILE_PATH = "./rootss.pem"
+CERTIFICATES_FILE_PATH = "./roots.pem"
 
 STATE_TOPIC = "state"
 DEFAULT_TOPIC = "events"
